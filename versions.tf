0e7e5bbd18dcff50ee292c9f32122661d5ca126c
Arthur Diniz <arthurbdiniz@gmail.com>
2020-11-04 23:07:23 -0300

    Lock terraform version at 0.13
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- versions.tf
+++ versions.tf
@@ -1,12 +1,10 @@
 terraform {
-  required_version = "~> 0.13.0"
+  required_version = "~> 0.13"
 
   required_providers {
     aws              = ">= 2.0, < 4.0"
     helm             = ">= 1.0, < 1.4.0"
     kubernetes       = ">= 1.10.0"
-    local            = "~> 1.2"
-    null             = "~> 2.0"
     kubernetes-alpha = "~> 0.2.1"
   }
 }
