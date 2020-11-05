5b204c4ffc611e1f64478c4dd89d1e72c8eba4bf
Arthur Diniz <arthurbdiniz@gmail.com>
2020-11-04 23:12:45 -0300

    Update aws provider to ">= 3.13, < 4.0"
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 0.13"
 
   required_providers {
-    aws              = ">= 2.0, < 4.0"
+    aws              = ">= 3.13, < 4.0"
     helm             = ">= 1.0, < 1.4.0"
     kubernetes       = ">= 1.10.0"
     kubernetes-alpha = "~> 0.2.1"
