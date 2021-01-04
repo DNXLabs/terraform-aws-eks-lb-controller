3dca029395b225f6b6672e93f8cbad4ca69b0a40
Allan Denot <adenot@gmail.com>
2021-01-04 12:56:46 +1100

    Updating version to 1.1.0

--- _variables.tf
+++ _variables.tf
@@ -39,7 +39,7 @@ variable "helm_chart_repo" {
 
 variable "helm_chart_version" {
   type        = string
-  default     = "1.0.3"
+  default     = "1.1.0"
   description = "AWS Load Balancer Controller Helm chart version."
 }
 
