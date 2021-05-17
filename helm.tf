63e1abab68a442f98c10213dbc1fa520877f9a13
Arthur Diniz <arthurbdiniz@gmail.com>
2021-05-17 17:18:18 -0300

    Upgrade crds deployment to kubeclt provider
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- helm.tf
+++ helm.tf
@@ -1,5 +1,5 @@
 resource "helm_release" "lb_controller" {
-  depends_on = [var.mod_dependency, kubernetes_manifest.crds, kubernetes_namespace.lb_controller]
+  depends_on = [var.mod_dependency, kubectl_manifest.crds, kubernetes_namespace.lb_controller]
   count      = var.enabled ? 1 : 0
   name       = var.helm_chart_name
   chart      = var.helm_chart_release_name
