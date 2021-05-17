ce5d101a85d646d688ad85dadb453fb395e26aaf
Arthur Diniz <arthurbdiniz@gmail.com>
2021-05-17 17:18:02 -0300

    Update helm, kubernetes and kubeclt providers
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- versions.tf
+++ versions.tf
@@ -2,9 +2,12 @@ terraform {
   required_version = "~> 0.13"
 
   required_providers {
-    aws              = ">= 3.13, < 4.0"
-    helm             = ">= 1.0, < 1.4.0"
-    kubernetes       = ">= 1.10.0"
-    kubernetes-alpha = "~> 0.2.1"
+    aws        = ">= 3.13, < 4.0"
+    helm       = ">= 1.0, < 3.0"
+    kubernetes = ">= 1.10.0, < 3.0.0"
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.10.0"
+    }
   }
 }
