25e65d808ec767ccc2decb2271808fc509e741de
Arthur Diniz <arthurbdiniz@gmail.com>
2021-11-03 17:30:41 -0300

    Remove crds and kubectl provider
    - CRDs will be installed along with Helm v2
    
    Closes: #2
    
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- versions.tf
+++ versions.tf
@@ -5,9 +5,5 @@ terraform {
     aws        = ">= 3.13, < 4.0"
     helm       = ">= 1.0, < 3.0"
     kubernetes = ">= 1.10.0, < 3.0.0"
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.9.4"
-    }
   }
 }
