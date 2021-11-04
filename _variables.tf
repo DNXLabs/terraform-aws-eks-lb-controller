a6c9f16b655686d360767f4895e48f05e8bc4f4d
Arthur Diniz <arthurbdiniz@gmail.com>
2021-11-04 19:57:35 -0300

    Run terraform fmt
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- _variables.tf
+++ _variables.tf
@@ -74,7 +74,7 @@ variable "mod_dependency" {
 }
 
 variable "settings" {
-  type        = map
+  type        = map(any)
   default     = {}
   description = "Additional settings which will be passed to the Helm chart values, see https://github.com/aws/eks-charts/tree/master/stable/aws-load-balancer-controller#configuration."
 }
