89e0863254bc9f027e08c939226c61aa32974d61
Bruno Meneguello <bruno.meneguello@dnx.solutions>
2022-08-25 19:04:53 -0300

    Upgrades the aws-load-balancer-controller Helm chart to v1.4.4
    Also updates the example to work after the latest changes to dependencies and adds an example Kubernetes Service to test the LoadBalancer configuration.

--- examples/basic/eks.tf
+++ examples/basic/eks.tf
@@ -82,6 +82,8 @@ module "vpc" {
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 17.0"
+
   cluster_name    = local.cluster_name
   cluster_version = "1.21"
   subnets         = module.vpc.private_subnets
