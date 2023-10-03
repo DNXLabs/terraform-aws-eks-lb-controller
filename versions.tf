1d9d9f87cbc0aadb2c7309a95b998ee880543d05
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2023-10-03 16:38:46 +1100

    fix lint

--- versions.tf
+++ versions.tf
@@ -2,9 +2,18 @@ terraform {
   required_version = ">= 0.13"
 
   required_providers {
-    aws        = ">= 3.35"
-    helm       = ">= 1.0, < 3.0"
-    kubernetes = ">= 1.10.0, < 3.0.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.35"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 1.0, < 3.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 1.10.0, < 3.0.0"
+    }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.9.4"
